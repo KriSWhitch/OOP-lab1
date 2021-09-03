4ed27b80000eb1e009e0825fd8ec3204127bd9b7
Alexei Mukovozchik <krisfragerfeed@gmail.com>
2021-09-03 18:00:41 +0300

    Initial commit

---
+++ app/src/main/java/by/bstu/fit/mukovozchykaliaksei/MainActivity.java
@@ -0,0 +1,15 @@
+package by.bstu.fit.mukovozchykaliaksei;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // TODO add new function
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
